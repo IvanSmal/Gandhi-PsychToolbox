1269d1c305900200949e3b6415637bbcf9719eea
IvanSm <smalianchuk.ivan@gmail.com>
2023-01-13 12:39:05 -0500

    Live target updates

--- Main code/background functions/Screen2.m
+++ Main code/background functions/Screen2.m
@@ -1,16 +1,16 @@
-function Screen2(a,internal,varargin)
-if internal.diode_on
-    Screen('FillRect', internal.window_main, internal.diode_color, internal.diode_pos);
+function Screen2(a,in,varargin)
+if in.diode_on
+        Screen('FillRect', in.window_main, in.diode_color, in.diode_pos);
 end
 
 if nargin==2
-    Screen(a, internal.window_main);
-    Screen(a, internal.window_monitor);
+    Screen(a, in.window_main);
+    Screen(a, in.window_monitor);
 else
-    Screen(a, internal.window_main,varargin{:});
-    Screen(a, internal.window_monitor,varargin{1},varargin{2}/4,varargin{3:end});
+    Screen(a, in.window_main,varargin{:});
+    Screen(a, in.window_monitor,varargin{1},varargin{2}/4,varargin{3:end});
 end
 
 %% extra stuff on monitor screen here
-Screen('DrawDots', internal.window_monitor, internal.eye.geteye/4, 10 , [1,1,1]);
+Screen('DrawDots', in.window_monitor, in.eye.geteye/4, 10 , [1,1,1]);
 end

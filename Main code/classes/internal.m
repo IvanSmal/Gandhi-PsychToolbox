e915a9ce0b3bdd0a13a4ac059272ab15f1d3c289
IvanSm <smalianchuk.ivan@gmail.com>
2023-05-30 17:54:24 -0400

    basic collision

--- Main code/classes/internal.m
+++ Main code/classes/internal.m
@@ -133,6 +133,23 @@ classdef internal < handle
         function getmovie(obj, moviepath,varargin)
             obj.movie=Screen('OpenMovie',obj.window_main,moviepath,varargin{:});
         end
+
+        function out=targcollisioncheck(obj,t1,t2)
+            t1pos=obj.trialtarg(t1,'getpos');
+            t2pos=obj.trialtarg(t2,'getpos');
+            
+            t1x=t1pos(1):t1pos(3);
+            t2x=t2pos(1):t2pos(3);
+
+            t1y=t1pos(2):t1pos(4);
+            t2y=t2pos(2):t2pos(4);
+
+            if any(ismember(t1x,t2x)) && any(ismember(t1y,t2y))
+                out=1;
+            else
+                out=0;
+            end
+        end
         %% methods from old "experiment" structure
         function addtarg(e,name,varargin)
             outcells={'name', name, varargin{:}};

058c58a1bc23413dde0b2a3ca62fde1a9b70d71e
gandhilab <gandhilab@gandhilab>
2023-06-01 09:59:15 -0400

    big changes to run on linux

--- Main code/background functions/Screen2.m
+++ Main code/background functions/Screen2.m
@@ -1,46 +1,46 @@
 function out=Screen2(a,mh,varargin)
+
 if nargin>1
     checkint2=inputname(2);
     if matches(checkint2,'mh')
-        if mh.diode_on
-            Screen('FillRect', mh.window_main, mh.diode_color, mh.diode_pos);
-        end
-        try
-            Screen(a, mh.window_main,varargin{:});
-        catch
-            out=Screen(a, mh.window_main,varargin{:});
-        end
-        try
-            if matches(a,'DrawTexture','IgnoreCase',1)
-                if nargin==3
-                    Screen(a, mh.window_monitor,varargin{1},mh.windowRect,mh.monitor_rect)
-                else
-                    Screen(a, mh.window_monitor,varargin{1},varargin{2},varargin{3}/4,varargin{4:end});
-                end
-            else
-                try
-                    Screen(a, mh.window_monitor,varargin{1},varargin{2}/4,varargin{3:end});
-                catch
-                    Screen(a, mh.window_monitor,varargin{1},varargin{2}/4)
-                    disp('here')
-                end
+        if ~contains(a,'movie','IgnoreCase',1)
+            if mh.diode_on
+                Screen('FillRect', mh.window_main, mh.diode_color, mh.diode_pos);
+                Screen('FillRect', mh.window_monitor, mh.diode_color, mh.diode_pos);
             end
-        catch
-            Screen('TextSize', mh.window_monitor,9);
-            Screen('DrawText', mh.window_monitor, 'couldn''t draw something in monitor window', 5, 12 , [1,1,1]);
+            %this try-catch will do stuff for main window. Basically
+            %untouched pass from Screen() to Screen2()
+            try
+                Screen(a, mh.window_main,varargin{:});
+                Screen(a, mh.window_monitor,varargin{:});
+            catch
+                out=Screen(a, mh.window_main,varargin{:});
+                out=Screen(a, mh.window_monitor,varargin{:});
+            end
+        else
+            try
+                out=Screen(a, mh.window_main,varargin{:});
+                disp(here)
+            catch
+                Screen(a, mh.window_main,varargin{:});
+            end
+            %% tell user that the movie is on
+            Screen('TextSize', mh.window_monitor,20);
+            Screen('DrawText', mh.window_monitor, 'MOVIE PLAYING', mh.xCenter, mh.yCenter , [255,255,255]);
         end
-        %% extra stuff on monitor screen here
-        Screen('DrawDots', mh.window_monitor, mh.eye.geteye/4, 3 , [1,1,1]);
-        Screen('TextSize', mh.window_monitor,9);
-        Screen('DrawText', mh.window_monitor, mh.activestatename, 5, 5 , [1,1,1]);
+        %% extra stuff on monitor
+        eyepos=mh.eye.geteye;
+        Screen('DrawDots', mh.window_monitor, eyepos, 10 , [255,255,255]);
+        Screen('TextSize', mh.window_monitor,20);
+        Screen('DrawText', mh.window_monitor, mh.activestatename, 5, 5 , [255,255,255]);
+        Screen('DrawText', mh.window_monitor, num2str(eyepos), mh.xCenter, 5 , [255,255,255]);
     else
         try
-            out=Screen(a, mh,varargin{:});
+            varargout=Screen(a, mh,varargin{:});
         catch
             Screen(a, mh,varargin{:});
         end
     end
 else
     Screen(a);
-end
 end

074828abd183f5c49597a5d835a8059a42b55826
IvanSm <smalianchuk.ivan@gmail.com>
2024-02-20 11:52:12 -0500

    Technically textures should work

--- Main code/classes/experiment.m
+++ Main code/classes/experiment.m
@@ -2,11 +2,10 @@ classdef experiment
     
     properties
         subject_name
-        dir
-        parameterfile
+        DataDir
+        parameter_file
         TrellisDir
         trial=struct(trial); % individual trial info e.g. which targets were actually used. this is the one that will actually iterate
-    
     end
 
     methods 

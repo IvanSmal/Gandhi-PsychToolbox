9cc07ee23752a84de7bf4450fc9375ae04c07c58
gandhi <gandhi@Rig1>
2024-08-13 15:44:06 -0400

    added files

---
+++ Main code/background_functions/setupEyelink.m
@@ -0,0 +1,35 @@
+function out = setupEyelink(app,first)
+%SETUPEYELINK Summary of this function goes here
+%   Detailed explanation goes here
+if first
+    try
+    Eyelink('SetAddress','192.168.42.3');
+    Eyelink('initialize');
+    catch
+        insToTxtbox(app,'Can''t connect to EyeLink. Is it on?')
+    end
+end
+try
+rec=Eyelink('CheckRecording');
+if rec ~=0
+    app.EyeLinkLamp.Color=[1,1,0];
+    if app.checkEyeLink
+    insToTxtbox(app,'Eyelink not in "record" mode')
+    app.checkEyeLink=0;
+    end
+elseif rec ==0
+    app.checkEyeLink=1;
+    app.EyeLinkLamp.Color=[0,1,0];
+end
+catch
+    app.EyeLinkLamp.Color=[1,0,0];
+    try
+        Eyelink('SetAddress','192.168.42.3');
+    end
+    try
+        Eyelink('initialize');
+    end
+end
+end
+
+
